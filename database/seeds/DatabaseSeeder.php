c81127166b711230d51a6cab675c4883a463d914
Riva'i Amin <rivaiamin@mhs.uinjkt.ac.id>
2016-04-18 14:05:38 +0700

    fixing database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(RoleUserTableSeeder::class);
+        $this->call(ProvinceTableSeeder::class);
+        $this->call(CityTableSeeder::class);
     }
 }
