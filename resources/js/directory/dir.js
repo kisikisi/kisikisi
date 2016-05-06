356ebd6660a715e4e668e3af5b2357d70b41a69c
Riva'i Amin <rivaiamin@mhs.uinjkt.ac.id>
2016-05-06 10:44:23 +0700

    School Directory front-end

---
+++ resources/js/directory/dir.js
@@ -0,0 +1,14 @@
+var dirCtrl = ['$http','$scope', '$rootScope', '$location', 'Notification', 'envService', 
+function($http, $scope, $rootScope, $location, Notification, envService) {
+    $rootScope.env = envService.read('all');
+    //console.log($rootScope.env);
+    $scope.onDetail = false;
+    
+    $scope.listSchool = function() {
+        $http.get($scope.env.api+'school')
+        .success(function (response) {
+            $scope.school = response.school;
+        })
+    }    
+    $scope.listSchool();
+}];
--- resources/views/directory.blade.php
+++ resources/views/directory.blade.php
@@ -1,156 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app="kisiDirApp">
 <head>
     <meta charset="UTF-8">
     <title>Direktori Sekolah</title>
     <link rel="stylesheet" href="{{ asset('css/directory.min.css') }}">
+    <base href="/index.php"></base>
     <script src="{{ asset('js/lib.directory.min.js') }}"></script>
+    <script src="{{ asset('js/directory.min.js') }}"></script>
 </head>
-<body>
-    <div class="ui secondary pointing menu">
-      <div class="ui container">
-          <a class="active item">
-            Home
-          </a>
-          <a class="item">
-            Messages
-          </a>
-          <a class="item">
-            Friends
-          </a>
-          <div class="right menu">
-            <a class="ui item">
-              Logout
-            </a>
-          </div>
-      </div>
-    </div>
+<body ng-controller="dirCtrl">
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=1496399374007633";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    
+    <?php include(public_path('views/directory/navbar.html')) ?> <!-- main navbar -->
+    
     <div class="ui container">
         <div class="ui grid">
          <div class="twelve wide column">
-             <div class="right ui rail" style="min-height: 263px;">
-               <div class="ui sticky" >
-                 <h3 class="ui header">Stuck Content</h3>
-                 <img>
-               </div>
-             </div>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
-              <p>&nbsp;</p>
+             
+            <?php include(public_path('views/directory/sidebar.html')) ?> <!-- main navbar -->
+            
+            <div ui-view></div>
+            
+            <div id="content" class="ui three cards" ng-include="'views/directory/school.list.html'">
+
+            </div>
          </div>
         </div>        
     </div>
 <!--    <div class="ui container">
         
-        <div id="content" class="ui four cards">
-         <div class="right ui rail" style="min-height: 263px;">
-            <div class="ui sticky" style="width: 272px !important; height: 263px !important; left: 1080.5px;">
-              <h3 class="ui header">Stuck Content</h3>
-              <img>
-            </div>
-        </div>
-         <div class="card">
-          <div class="image">
-            <img src="/img/avatar.png">
-          </div>
-          <div class="content">
-            <a class="header">Kristy</a>
-            <div class="meta">
-              <span class="date">Joined in 2013</span>
-            </div>
-            <div class="description">
-              Kristy is an art director living in New York.
-            </div>
-          </div>
-          <div class="extra content">
-            <a>
-              <i class="user icon"></i>
-              22 Friends
-            </a>
-          </div>
-         </div>
-         <div class="card">
-          <div class="image">
-            <img src="/img/avatar2.png">
-          </div>
-          <div class="content">
-            <a class="header">Kristy</a>
-            <div class="meta">
-              <span class="date">Joined in 2013</span>
-            </div>
-            <div class="description">
-              Kristy is an art director living in New York.
-            </div>
-          </div>
-          <div class="extra content">
-            <a>
-              <i class="user icon"></i>
-              22 Friends
-            </a>
-          </div>
-         </div>
-         <div class="card">
-          <div class="image">
-            <img src="/img/avatar3.png">
-          </div>
-          <div class="content">
-            <a class="header">Kristy</a>
-            <div class="meta">
-              <span class="date">Joined in 2013</span>
-            </div>
-            <div class="description">
-              Kristy is an art director living in New York.
-            </div>
-          </div>
-          <div class="extra content">
-            <a>
-              <i class="user icon"></i>
-              22 Friends
-            </a>
-          </div>
-         </div>
-         <div class="card">
-          <div class="image">
-            <img src="/img/avatar5.png">
-          </div>
-          <div class="content">
-            <a class="header">Kristy</a>
-            <div class="meta">
-              <span class="date">Joined in 2013</span>
-            </div>
-            <div class="description">
-              Kristy is an art director living in New York.
-            </div>
-          </div>
-          <div class="extra content">
-            <a>
-              <i class="user icon"></i>
-              22 Friends
-            </a>
-          </div>
-         </div>
-        </div>
     </div>-->
     <script>
         $('.ui.sticky').sticky();
+        $('.menu .item').tab();
+        $('.ui.rating').rating();
     </script>
 </body>
 </html>
