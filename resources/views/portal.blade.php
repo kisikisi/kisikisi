ca0a92e6d44acc4ec640bba493729d927b2d8b01
Arbiyanto Wijaya <arbiyantowijaya17@yahoo.co.id>
2016-05-13 00:39:31 +0700

    add typed.js ,revisi portal, image

--- resources/views/portal.blade.php
+++ resources/views/portal.blade.php
@@ -39,15 +39,16 @@
 </div>
 
 <div id="section-min-1">
-	<div class="ui container">
-		<img src="{{asset('img/icon/svg icon/chat_baloon.svg')}}" class="baloon" />
-		<h1>Latar Belakang</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<div class="ui grid container">
+		<div class="six wide column">
+			<h1 class="title">Latar Belakang</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
 	</div>
 </div>
 
